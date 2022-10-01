e6648c382a6ecb72a4a031c0fa8eb64b1e0c87d7
nicolasceccato <nicolasceccato@hotmail.com>
2022-10-01 14:50:28 -0300

    Inverte String utilizando pilha

---
+++ Pilhas/exercicio3/Pilha.h
@@ -0,0 +1,24 @@
+#define ERRO_PILHA_VAZIA -1
+#define ERRO_PILHA_CHEIA -2
+
+typedef struct{
+    int *dados;
+    int topo;
+    int capacidade;
+} Pilha;
+
+void inicializa_pilha (Pilha *p, int c);
+
+int pilha_vazia (Pilha p);
+
+int pilha_cheia (Pilha p);
+
+int empilha (Pilha *p, int info);
+
+int desempilha (Pilha *p, int *info);
+
+int le_topo(Pilha p, int *info);
+
+void mostra_pilha(Pilha p);
+
+void desaloca_pilha (Pilha *p);
---
+++ Pilhas/exercicio3/README.md
@@ -0,0 +1,3 @@
+Escreva uma função (em nível de aplicação, ou seja, no arquivo principal do programa)
+que utiliza uma pilha para inverter um string. Para isso, basta empilhar todos os seus
+caracteres em uma pilha e, em seguida, desempilhar de volta na string.
